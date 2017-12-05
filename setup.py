0ea7ba488b229ec710d77d0213b11c9417763db5
Alex Seeholzer <seeholzer@gmail.com>
2017-12-06 00:31:05 +0100

    updated setup to work as intended

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from distutils.extension import Extension
+from pip.req import parse_requirements
 
 import Cython.Compiler.Options
 import cython_gsl
 import numpy
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 # Generate compiler annotations in HTML
 Cython.Compiler.Options.annotate = True
 
 ext_modules = [
     Extension(
-        "ciles",
+        "ciles.ciles",
         ["ciles/ciles.pyx"],
         include_dirs=[
             numpy.get_include(), cython_gsl.get_cython_include_dir()],
@@ -20,10 +21,13 @@ ext_modules = [
     )
 ]
 
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
-    cmdclass={'build_ext': build_ext},
     include_dirs=[cython_gsl.get_include()],
-    ext_modules=ext_modules,
+    ext_modules=cythonize(ext_modules),
+    packages=find_packages(),
     test_suite='tests',
     name='ciles',
     description='Langevin integrator for SDEs with constant drift and diffusion on continuous intervals with circular boundary conditions.',
@@ -37,5 +41,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Medical Science Apps.'],
     license='GPLv2',
-    version='0.1.0'
+    version='0.1.0',
+    install_requires=reqs,
+    setup_requires=reqs
 )
